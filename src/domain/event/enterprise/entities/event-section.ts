4bcbdf7feefef634463a3b17d96a44020d44dd5a
chain <klaus.mika1052@gmail.com>
2023-12-20 17:18:36 -0300

    refact: extends to section entity

--- src/domain/event/enterprise/entities/event-section.ts
+++ src/domain/event/enterprise/entities/event-section.ts
@@ -1,51 +1,19 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
-import { Optional } from '@/core/types/optional'
+import { Section, SectionProps } from './section'
 
-interface EventSectionProps {
-  name: string
-  description: string | null
-  totalSpots: number
-  price: number
+interface EventSectionProps extends SectionProps {
+  eventId: UniqueEntityId
 }
 
-export class EventSection extends Entity<EventSectionProps> {
-  get name() {
-    return this.props.name
+export class EventSection extends Section<EventSectionProps> {
+  get eventId() {
+    return this.props.eventId
   }
 
-  set name(name: string) {
-    this.props.name = name
-  }
-
-  get description() {
-    return this.props.description
-  }
-
-  set description(description: string | null) {
-    this.props.description = description
-  }
-
-  get totalSpots() {
-    return this.props.totalSpots
-  }
-
-  get price() {
-    return this.props.price
-  }
-
-  set price(price: number) {
-    this.props.price = price
-  }
-
-  static create(
-    props: Optional<EventSection, 'description'>,
-    id?: UniqueEntityId,
-  ) {
+  static create(props: EventSectionProps, id?: UniqueEntityId) {
     const eventSection = new EventSection(
       {
         ...props,
-        description: props.description ?? null,
       },
       id,
     )
