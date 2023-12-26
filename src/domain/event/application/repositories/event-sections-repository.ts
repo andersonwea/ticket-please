986e37e784cd2c7e825cacc930e991a54e3d65e6
chain <klaus.mika1052@gmail.com>
2023-12-26 15:31:12 -0300

    feat: create method to find by id

--- src/domain/event/application/repositories/event-sections-repository.ts
+++ src/domain/event/application/repositories/event-sections-repository.ts
@@ -2,5 +2,6 @@ import { EventSection } from '../../enterprise/entities/event-section'
 
 export interface EventSectionsRepository {
   create(eventSection: EventSection): Promise<void>
+  findById(id: string): Promise<EventSection | null>
   findManyByEventId(eventId: string): Promise<EventSection[]>
 }
--- src/test/repositories/in-memory-event-sections-repository.ts
+++ src/test/repositories/in-memory-event-sections-repository.ts
@@ -10,6 +10,16 @@ export class InMemoryEventSectionsRepository
     this.items.push(eventSection)
   }
 
+  async findById(id: string): Promise<EventSection | null> {
+    const eventSection = this.items.find((item) => item.id.toString() === id)
+
+    if (!eventSection) {
+      return null
+    }
+
+    return eventSection
+  }
+
   async findManyByEventId(eventId: string): Promise<EventSection[]> {
     const eventSections = this.items.filter(
       (eventSection) => eventSection.eventId.toString() === eventId,
