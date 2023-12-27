1db950effa9282088e6f4dda7c2ba8a7116212d5
chain <klaus.mika1052@gmail.com>
2023-12-27 11:10:26 -0300

    refact: remove unless import

--- src/domain/event/application/use-cases/get-event-by-id.spec.ts
+++ src/domain/event/application/use-cases/get-event-by-id.spec.ts
@@ -1,5 +1,4 @@
 import { InMemoryEventsRepository } from '@/test/repositories/in-memory-events-repository'
-import { makePartner } from '@/test/factories/make-partner'
 import { GetEventByIdUseCase } from './get-event-by-id'
 import { makeEvent } from '@/test/factories/make-event'
 
