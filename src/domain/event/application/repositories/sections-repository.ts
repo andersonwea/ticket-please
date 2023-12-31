21a768e4c65073938bedc97371ec1553d9b219be
chain <klaus.mika1052@gmail.com>
2023-12-31 15:34:47 -0300

    refact: remove unless repository

--- src/domain/event/application/repositories/sections-repository.ts
+++
@@ -1,6 +0,0 @@
-import { Section, SectionProps } from '../../enterprise/entities/section'
-
-export interface SectionsRepository {
-  create(section: Section<SectionProps>): Promise<void>
-  findManyByEventId(id: string): Promise<Section<SectionProps>[]>
-}
