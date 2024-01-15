edba00298bacd1ca06d745bf9de9e28ccd27a975
chain <klaus.mika1052@gmail.com>
2024-01-15 12:18:31 -0300

    feat: create date changed event

---
+++ src/domain/event/enterprise/events/event-changed-date-event.ts
@@ -0,0 +1,19 @@
+import { DomainEvent } from '@/core/events/domain-event'
+import { Event } from '../entities/event'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+export class EventChangedDateEvent implements DomainEvent {
+  public ocurredAt: Date
+  public event: Event
+  public date: Date
+
+  constructor(event: Event, date: Date) {
+    this.ocurredAt = new Date()
+    this.event = event
+    this.date = date
+  }
+
+  getAggregateId(): UniqueEntityId {
+    return this.event.id
+  }
+}
