bf0d27d4e024fb0edbdc01328ad5a678ac97f6ea
chain <klaus.mika1052@gmail.com>
2024-01-15 17:53:43 -0300

    feat: add new method

--- src/test/repositories/in-memory-orders-repository.ts
+++ src/test/repositories/in-memory-orders-repository.ts
@@ -1,10 +1,25 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { OrdersRepository } from '@/domain/event/application/repositories/orders-repository'
-import { Order } from '@/domain/event/enterprise/entities/order'
+import { Order, OrderStatus } from '@/domain/event/enterprise/entities/order'
 
 export class InMemoryOrdersRepository implements OrdersRepository {
   items: Order[] = []
 
+  async findById(id: string): Promise<Order | null> {
+    const order = this.items.find((item) => item.id.toString() === id)
+
+    if (!order) {
+      return null
+    }
+
+    return order
+  }
+
   async create(order: Order): Promise<void> {
+    if (order.status === OrderStatus.PAID) {
+      DomainEvents.dispatchEventsForAggregate(order.id)
+    }
+
     this.items.push(order)
   }
 }
