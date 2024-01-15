11d06badaa292e25a635b160bf74ab36bfaf1a60
chain <klaus.mika1052@gmail.com>
2024-01-15 12:19:33 -0300

    feat: create email entity

---
+++ src/domain/email/enterprise/entities/email.ts
@@ -0,0 +1,57 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+export interface EmailProps {
+  recipientId: UniqueEntityId
+  subject: string
+  content: string
+  createdAt: Date
+  sentAt?: Date
+}
+
+export class Email extends Entity<EmailProps> {
+  get recipientId() {
+    return this.props.recipientId
+  }
+
+  get subject() {
+    return this.props.subject
+  }
+
+  set subject(subject: string) {
+    this.props.subject = subject
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  set content(content: string) {
+    this.props.content = content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get sentAt() {
+    return this.props.sentAt
+  }
+
+  send() {
+    this.props.sentAt = new Date()
+  }
+
+  static create(props: Optional<EmailProps, 'createdAt'>, id?: UniqueEntityId) {
+    const email = new Email(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return email
+  }
+}
